30479c19eb2b93392b8f617826063ca8b0dc7f33
edyta <siwkoedyta@gmail.com>
2023-05-31 21:32:49 +0200

    everything

--- src/main/java/efs/task/reflection/model/Villager.java
+++ src/main/java/efs/task/reflection/model/Villager.java
@@ -1,4 +1,6 @@
 package efs.task.reflection.model;
+import efs.task.reflection.annotations.NotNull;
+import efs.task.reflection.annotations.BuilderProperty;
 
 /**
  * TODO Dodaj w odpowiednich miejscach adnotacje utworzone w pakiecie
@@ -12,10 +14,12 @@ public class Villager implements Fighter {
   public static final String HIDDEN_VILLAGER_NAME = "Anonymous";
   public static final String HIDDEN_VILLAGER_DESC = "Hidden from the world, can hack anything that uses electricity";
 
+  @NotNull
   private final String name;
 
+  @NotNull
   private final String description;
-  
+
   private Integer age;
 
   int health;
@@ -23,22 +27,26 @@ public class Villager implements Fighter {
   private Villager() {
     this(HIDDEN_VILLAGER_NAME, HIDDEN_VILLAGER_DESC);
   }
-  
-  private Villager(Integer age , String name) {
+
+  @BuilderProperty(name = "init")
+  private Villager(Integer age , @NotNull String name) {
     this(name, HIDDEN_VILLAGER_DESC);
     this.age = age;
   }
 
-  private Villager(String name, Integer age) {
+  @BuilderProperty(name = "init")
+  private Villager(@NotNull String name, Integer age) {
     this(name, HIDDEN_VILLAGER_DESC);
     this.age = age;
   }
 
-  public Villager(final String name, final String description) {
+  @BuilderProperty(name = "init")
+  public Villager(@NotNull final String name, @NotNull final String description) {
     this.name = name;
     this.description = description;
   }
 
+  @BuilderProperty(name = "health")
   public void setHealth(int health) {
     this.health = health;
   }
